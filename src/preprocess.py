a48c3b2f3c9916b26c6e906b672f5e1d5efc437d
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-01-26 21:53:41 +0100

    feat: add training script with MLflow tracking, pipeline integration, and conditional model registration

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,44 +6,31 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
 
-NON_ZERO_COLUMNS = [
-    'Glucose', 
-    'BloodPressure', 
-    'SkinThickness', 
-    'Insulin', 
-    'BMI'
-]
+PHYSIOLOGICAL_COLS = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
 
-
-class ZeroToNan(BaseEstimator, TransformerMixin) :
+class ZeroToNaN(BaseEstimator, TransformerMixin):
     def __init__(self, columns=None):
         self.columns = columns
-    
-    def fit(self, X, y=None) :
+
+    def fit(self, X, y=None):
         return self
-    
-    def transform(self, X) :
-        X_copy = X.copy
+
+    def transform(self, X):
+        X_copy = X.copy()
         
-        if isinstance(X_copy, pd.DataFrame) :
-            for col in self.columns :
-                if col in X_copy.columns :
-                    X_copy[col] = X_copy[col].replace(0, np.nan)
-            
+        if isinstance(X_copy, pd.DataFrame):
+            for col in self.columns:
+                if col in X_copy.columns:
+                    X_copy[col] = pd.to_numeric(X_copy[col], errors='coerce')
+                    X_copy[col] = X_copy[col].mask(X_copy[col] == 0, np.nan)
+        
         return X_copy
+
+
+def create_pipeline():
     
-
-
-def create_pipeline() :
-    pipeline = Pipeline([
-        ('zero_to_nan', ZeroToNan(columns=NON_ZERO_COLUMNS)),
+    return Pipeline(steps=[
+        ('zero_to_nan', ZeroToNaN(columns=PHYSIOLOGICAL_COLS)),
         ('imputer', SimpleImputer(strategy='median')),
         ('scaler', StandardScaler())
     ])
-    
-    return pipeline
-
-
-if __name__ == "__main__" :
-    print("Pipeline définie avec succès !")
-    print("Columns :", NON_ZERO_COLUMNS)
---
+++ src/train.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, recall_score, f1_score
+from sklearn.pipeline import Pipeline
+from src.preprocess import create_pipeline
+
+
+RAW_DATA_PATH = "data/raw/data.csv"
+LABELED_DATA_PATH = "data/processed/data.csv"
+EXPERIMENT_NAME = "DiabOps_Experiment"
+MODEL_NAME = "diabrisk-model"
+
+def train():
+    print("Démarrage du script ...")
+    
+    try:
+        df_raw = pd.read_csv(RAW_DATA_PATH)
+        df_labeled = pd.read_csv(LABELED_DATA_PATH)
+        print(f"Chargé Raw: {df_raw.shape}, Labelled: {df_labeled.shape}")
+    except FileNotFoundError as e:
+        print(f"Erreur fichier: {e}")
+        return
+
+    drop_cols = ['Cluster', 'risk_category', 'Unnamed: 0']
+    
+    X_raw = df_raw.drop([c for c in drop_cols if c in df_raw.columns], axis=1)
+
+    X_labeled = df_labeled.drop([c for c in drop_cols if c in df_labeled.columns], axis=1)
+    
+    if "Cluster" not in df_labeled.columns:
+        print("Erreur: Colonne 'Cluster' manquante dans labeled_data.csv")
+        return
+    
+    y_labeled = df_labeled["Cluster"]
+
+    print(f"Features utilisées (Raw): {X_raw.shape}")
+    print(f"Features utilisées (Labelled): {X_labeled.shape}")
+
+    X_train_lbl, X_test_lbl, y_train_lbl, y_test_lbl = train_test_split(
+        X_labeled, y_labeled, test_size=0.2, random_state=42
+    )
+
+    mlflow.set_experiment(EXPERIMENT_NAME)
+    mlflow.sklearn.autolog(log_models=False)
+
+    with mlflow.start_run() as run:
+        print(f"Run ID: {run.info.run_id}")
+
+        print("Calibrage du pipeline de nettoyage sur X_raw...")
+        preprocessing_pipeline = create_pipeline()
+        preprocessing_pipeline.fit(X_raw)
+
+        print("Entraînement du SVC sur X_labeled...")
+        clf = SVC(C=10, kernel='linear', gamma='scale', probability=True, random_state=42)
+        clf.fit(X_train_lbl, y_train_lbl)
+
+        final_model = Pipeline([
+            ('preprocessor', preprocessing_pipeline),
+            ('classifier', clf)
+        ])
+
+        y_pred = clf.predict(X_test_lbl)
+        acc = accuracy_score(y_test_lbl, y_pred)
+        rec = recall_score(y_test_lbl, y_pred, average='macro')
+        f1 = f1_score(y_test_lbl, y_pred, average='macro')
+
+        print(f"Résultats :\n- Accuracy: {acc:.4f}\n- Recall: {rec:.4f}\n- F1: {f1:.4f}")
+        
+        mlflow.log_metrics({
+            "test_accuracy": acc,
+            "test_recall": rec,
+            "test_f1": f1
+        })
+
+        from mlflow.models.signature import infer_signature
+        
+        signature = infer_signature(X_raw.head(), clf.predict(X_train_lbl.head()))
+
+        if acc > 0.70:
+            print("Succès. Enregistrement dans le Registry.")
+            mlflow.sklearn.log_model(
+                sk_model=final_model,
+                artifact_path="model",
+                registered_model_name=MODEL_NAME,
+                signature=signature
+            )
+        else:
+            mlflow.sklearn.log_model(sk_model=final_model, artifact_path="model", signature=signature)
+
+
+
+
+if __name__ == "__main__":
+    train()
